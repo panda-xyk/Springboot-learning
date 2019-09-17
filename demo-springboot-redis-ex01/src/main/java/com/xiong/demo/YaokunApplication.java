86480993792c757c242003da863cc98b0cb55224
xiongyaokun <yaokun.xiong@nttdata.com>
2019-09-17 09:55:03 +0800

    增加一个redis的示例

---
+++ demo-springboot-redis-ex01/src/main/java/com/xiong/demo/YaokunApplication.java
@@ -0,0 +1,13 @@
+package com.xiong.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaokunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaokunApplication.class, args);
+	}
+
+}
